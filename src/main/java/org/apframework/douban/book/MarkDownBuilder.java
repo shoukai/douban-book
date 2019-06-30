e1d89c54b5fb158efe88fb7ab2562307c31bb004
Shoukai Huang <huangshoukai@renrenche.com>
2019-06-30 21:23:01 +0800

    fix formate

--- src/main/java/org/apframework/douban/book/MarkDownBuilder.java
+++ src/main/java/org/apframework/douban/book/MarkDownBuilder.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class MarkDownBuilder {
 
     static SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyy-MM-dd");
-    static String TEMPLATE = "| 名称：[%s](%s)<br>时间：%s<br>出版：%s<br>豆瓣：%s/%s<br>个人：%s<br>点评：%s<br>Tags：%s | ![](%s) |";
+    static String TEMPLATE = "| 名称：[%s](%s)<br>时间：%s<br>出版：%s<br>豆瓣：%s/%s<br>个人：%s<br>Tags：%s | ![](%s) |";
 
     public static String build(List<BookSummaryVO> summary) {
         Joiner joiner = Joiner.on("；").skipNulls();
@@ -26,12 +26,13 @@ public class MarkDownBuilder {
                             p.getAverageRating(),
                             p.getMaxRating(),
                             Strings.isNullOrEmpty(p.getMineRating()) ? "未评分" : p.getMineRating() + "/5",
-                            Strings.isNullOrEmpty(p.getComment()) ? "未评价" : p.getComment(),
+//                            Strings.isNullOrEmpty(p.getComment()) ? "未评价" : p.getComment(),
                             joiner.join(p.getTags()),
                             p.getImage()))
                     .append("\n");
         });
         return builder.toString();
     }
+
 
 }
